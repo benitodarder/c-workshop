b18dc103da7853b6ad33274a9abd13081d23fab8
benitodarder <benitodarder@gmail.com>
2023-01-22 22:31:07 +0100

    Using 1998 convention

--- freeMem/getFreeMem.c
+++ freeMem/getFreeMem.c
@@ -7,10 +7,11 @@ int main(int argc, char *argv[]) {
    
    printf("There we go...\n");
    freemem =  getTotalSystemMemory();
+   printf("Conversions applied following 1998 convention 1Kb = 10^3b\n");
    printf("Free memory in bytes: %lld\n", freemem);
-   printf("Free memory in kilobytes: %lld\n", freemem / 1024);
-   printf("Free memory in megabytes: %lld\n", freemem / 1048576);
-   printf("Free memory in gigabytes: %f\n", (float)(freemem / 1073741824));
+   printf("Free memory in kilobytes: %lld\n", freemem / 1000);
+   printf("Free memory in megabytes: %lld\n", freemem / 1000000);
+   printf("Free memory in gigabytes: %f\n", (float)(freemem / 1000000000));
    printf("That's all folks!\n");
    return 0;
 }
