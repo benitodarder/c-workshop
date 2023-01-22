9f076e5143722f1b9bdec5e06e0830bda71ef4d2
benitodarder <benitodarder@gmail.com>
2023-01-22 22:18:45 +0100

    Result in bytes/kb/mb/gb

--- freeMem/freemem.h
+++ freeMem/freemem.h
@@ -1,9 +1,11 @@
 /**
    freemem.h
    
-   Must declare IS_WINDOWS or not accordingly... 
+      Must declare IS_WINDOWS or not accordingly... As this depends heavily on the OS.
    
+   - getTotalSystemMemory() return system free memory in bytes.
+     
 **/
 
-
+/* Return system free memory in bytes. */
 unsigned long long getTotalSystemMemory();
--- freeMem/getFreeMem.c
+++ freeMem/getFreeMem.c
@@ -3,8 +3,14 @@
 #include "freemem.h"
 
 int main(int argc, char *argv[]) {
+   unsigned long long freemem;
+   
    printf("There we go...\n");
-   printf("Free memory: %lld\n", getTotalSystemMemory());
+   freemem =  getTotalSystemMemory();
+   printf("Free memory in bytes: %lld\n", freemem);
+   printf("Free memory in kilobytes: %lld\n", freemem >> 10);
+   printf("Free memory in megabytes: %lld\n", freemem >> 20);
+   printf("Free memory in gigabytes: %lld\n", freemem >> 30);
    printf("That's all folks!\n");
    return 0;
 }
