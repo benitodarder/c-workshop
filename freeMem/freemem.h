03841d6f8615a2eaea78bd29e5b776494de78e7f
benitodarder <benitodarder@gmail.com>
2023-01-22 22:52:03 +0100

    freemem.h commented

--- freeMem/freemem.h
+++ freeMem/freemem.h
@@ -1,10 +1,10 @@
 /**
-   freemem.h
-   
-      Must declare IS_WINDOWS or not accordingly... As this depends heavily on the OS.
-   
-   - getTotalSystemMemory() return system free memory in bytes.
-     
+ * freemem.h
+ * 
+ * Must declare IS_WINDOWS or not accordingly... As this depends heavily on the OS.
+ * 
+ * - getTotalSystemMemory() return system free memory in bytes.
+ * As described in https://stackoverflow.com/questions/2513505/how-to-get-available-memory-c-g
 **/
 
 /* Return system free memory in bytes. */
